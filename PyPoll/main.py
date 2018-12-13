d8a95c99f0146e27aa0d12a7f4eaa8cdbda43d87
jack heston <jheston2@gmail.com>
2018-12-13 13:10:03 -0700

    another commit

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,9 +1,20 @@
 import os
 import csv
 
+count = int()
+df = []
+
 csvpath = os.path.join("election_data.csv")
 with open(csvpath, newline="") as csvfile:
     csvreader = csv.reader(csvfile,delimiter=",")
     next(csvreader)
     for row in csvreader:
-        
+        df.append(row)
+
+    for row in df:
+        count += 1
+
+print(f"Election Results")
+print(f"----------------------")
+print(f"Total Votes: {count}")
+print(f"----------------------")
--- PyPoll/test.py
+++ PyPoll/test.py
@@ -1,14 +1,33 @@
 import os
 import csv
+import numpy as np
 
 count = int()
+df = []
+candidates = []
+candidates_unique = []
+khancount = int()
 
+# create list df
 csvpath = os.path.join("election_data.csv")
 with open(csvpath, newline="") as csvfile:
     csvreader = csv.reader(csvfile,delimiter=",")
     next(csvreader)
     for row in csvreader:
-        
-        count +=1
+        df.append(row)
 
-print(count)
+# create list of candidates
+candidates = [item[2] for item in df]
+
+# create list of unique candidates
+for x in candidates:
+    if x not in candidates_unique:
+        candidates_unique.append(x)
+
+#count votes for each candidate
+#candidate 1 - Khan
+for x in candidates:
+    if candidates == 'Khan':
+        khancount += 1
+
+print(head(candidates))
